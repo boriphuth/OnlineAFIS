e996878f2cc580c573f154b2cc1b6e3815f5a28d
boriphuth <savepoo@hotmail.com>
2013-09-24 18:00:00 +0700

    Add Afis interface and class

---
+++ OnlineAFIS/OnlineAFIS/AFIS/Afis.cs
@@ -0,0 +1,51 @@
+﻿using SourceAFIS.Simple;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineAFIS.AFIS
+{
+    public class Afis : IAfis
+    {
+        private Afis _instance;
+
+        // Lock synchronization object
+        private static object syncLock = new object();
+
+        // Constructor (protected)
+        public Afis()
+        {
+            if (_instance == null)
+            {
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Afis();
+                    }
+                }
+            }
+        }
+
+        public void Extract(Person person)
+        { 
+        
+        }
+
+        public List<Person> Identify(Person probe, List<Person> candidates)
+        {
+            List<Person> ListPerson = new List<Person>();
+
+
+            return ListPerson;
+        }
+
+        public float Verify(Person probe, Person candidate)
+        {
+            float verifyScore = 0;
+
+            return verifyScore;
+        }
+    }
+}
---
+++ OnlineAFIS/OnlineAFIS/AFIS/IAfis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SourceAFIS.Simple;
+
+namespace OnlineAFIS.AFIS
+{
+    interface IAfis
+    {
+        void Extract(Person person);
+        List<Person> Identify(Person probe, List<Person> candidates);
+        float Verify(Person probe, Person candidate);
+    }
+}
